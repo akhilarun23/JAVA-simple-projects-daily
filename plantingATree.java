24cca375f7b7093f8ade0fdf6a5515def41b5b6f
Akhil <141900915+akhilarun23@users.noreply.github.com>
2025-02-19 20:27:52 +0530

    Update plantingATree.java

--- plantingATree.java
+++ plantingATree.java
@@ -1,14 +1,29 @@
-public class Tree{
-  public static void main(String[] args){
-    //introduction and printing a tree
-    System.out.println("Hey there,I'm Akhil and I'm learning to code in Java");
-    System.out.println("We are going to plant a tree today");
-    System.out.println("*");
-    System.out.println("***");
-    System.out.println("***");
-    System.out.println("*");
-    System.out.println("*");
-    System.out.println("*");
+public class Tree {
+    public static void main(String[] args) throws InterruptedException {
+   
+        System.out.println("Hey there, I'm Akhil and I'm learning to code in Java!");
+        System.out.println("We are going to plant a tree today...\n");
 
-  }
+     
+        plantTree();
+    }
+
+
+    public static void plantTree() throws InterruptedException {
+        String[] tree = {
+            "   *   ",
+            "  ***  ",
+            " ***** ",
+            "*******",
+            "   *   ",
+            "   *   ",
+            "   *   "
+        };
+
+        
+        for (String line : tree) {
+            System.out.println(line);
+            Thread.sleep(500);
+        }
+    }
 }
