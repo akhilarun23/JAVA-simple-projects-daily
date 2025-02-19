35a1c2889f624d8ac5459b0fb52af50c39c7a967
Akhil <141900915+akhilarun23@users.noreply.github.com>
2025-02-19 20:25:11 +0530

    Create plantingATree.java

---
+++ plantingATree.java
@@ -0,0 +1,14 @@
+public class Tree{
+  public static void main(String[] args){
+    //introduction and printing a tree
+    System.out.println("Hey there,I'm Akhil and I'm learning to code in Java");
+    System.out.println("We are going to plant a tree today");
+    System.out.println("*");
+    System.out.println("***");
+    System.out.println("***");
+    System.out.println("*");
+    System.out.println("*");
+    System.out.println("*");
+
+  }
+}
