a50253bd1bdd72b098f7621ef77f9796d9d719d5
Akhil <141900915+akhilarun23@users.noreply.github.com>
2025-02-20 16:54:21 +0530

    Create MathMagic.java

---
+++ MathMagic.java
@@ -0,0 +1,16 @@
+public class Magic {
+	public static void main(String[] args) {
+       int myNumber = 3;
+       int m = 0 ;
+		   //myNumber is the original number
+       m *= myNumber;
+       m += myNumber;
+       m /= myNumber;
+       m += 17;
+       m -= myNumber;
+       m /=6;
+    
+       System.out.println(myNumber);
+    
+	}
+}
